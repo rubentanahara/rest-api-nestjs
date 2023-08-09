e82fde66fa608c0699c7afb7736cf3afa5478d10
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-08-09 14:27:15 -0700

    adding mongoose db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     BookModule,
   ],
   controllers: [AppController],
