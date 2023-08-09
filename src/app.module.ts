8afdad421bc0ea3dafea3c16722fbe0efae2ba55
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-08-09 14:25:40 -0700

    update:app.module
    adding dependencias and .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
-
 @Module({
-  imports: [BookModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
