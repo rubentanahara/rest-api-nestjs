14c7669ba812cf89d2ee0a47067e7717d6e6c2a4
Ruben Tanahara <rmartinez@gdlconnect.com>
2023-08-10 08:45:05 -0700

    update: Dto with decorators

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,9 +1,20 @@
+import { IsString, IsNumber, IsEnum, Min } from 'class-validator'; // Import the necessary decorators
 import { Category } from '../schemas/book.schema';
 
 export class CreateBookDto {
+  @IsString() // Validate that title is a string
   readonly title: string;
+
+  @IsString() // Validate that description is a string
   readonly description: string;
+
+  @IsString() // Validate that author is a string
   readonly author: string;
+
+  @IsNumber() // Validate that price is a number
+  @Min(0) // Validate that price is at least 0
   readonly price: number;
+
+  @IsEnum(Category) // Validate that category is one of the defined enum values
   readonly category: Category;
 }
